843d0efe7091a395d5010de8b9b97c70cc2dc2ed
Ewan Forbes <ewan_forbes@yahoo.co.uk>
2018-10-14 19:46:54 +0100

    Fixed endgame not happening glitch and other refactoring

--- js/app.js
+++ js/app.js
@@ -8,24 +8,17 @@ $(document).ready(() => {
   const keyMap = {
     87: false, 38: false, 68: false, 39: false, 65: false, 40: false, 83: false, 37: false, 71: false, 69: false, 80: false,
   };
-  const asteroids = [];
-  const bullets = [];
-  let asteroidImg;
-  let paused = false;
-  let playing = false;
-  let splashImg;
-  let player;
 
   $(document).keydown((e) => {
     if (e.keyCode in keyMap) {
       keyMap[e.keyCode] = true;
-      if (paused && e.keyCode === 80) {
-        paused = false;
-      } else if (!paused && e.keyCode === 80) {
-        paused = true;
+      if (gameArea.paused && e.keyCode === 80) {
+        gameArea.paused = false;
+      } else if (!gameArea.paused && e.keyCode === 80) {
+        gameArea.paused = true;
       }
-      if (!playing && e.keyCode === 69) {
-        playing = true;
+      if (!gameArea.playing && e.keyCode === 69) {
+        gameArea.playing = true;
       }
     }
   }).keyup((e) => {
@@ -38,20 +31,27 @@ $(document).ready(() => {
     constructor() {
       this.update = this.update.bind(this);
       this.clear = this.clear.bind(this);
+      this.startScreen = this.startScreen.bind(this);
       this.canvas = document.createElement('canvas');
-      this.canvas.width = gameWidth;
-      this.canvas.height = gameHeight;
+      this.canvas.width = $(window).width() - 40;
+      this.canvas.height = $(window).height() - 40;
       this.context = this.canvas.getContext('2d');
       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
       this.interval = setInterval(this.update, 20);
 
-      player = new Player();
-      asteroidImg = new Image();
-      asteroidImg.src = 'assets/asteroid.png';
-      splashImg = new Image();
-      splashImg.src = 'assets/splash.png';
+      this.paused = false;
+      this.playing = false;
+
+      this.asteroids = [];
+      this.bullets = [];
+
+      this.asteroidImg = new Image();
+      this.asteroidImg.src = 'assets/asteroid.png';
+      this.player = new Player();
+      this.splashImg = new Image();
+      this.splashImg.src = 'assets/splash.png';
       for (let i = 0; i < maxAsteroids; i++) {
-        asteroids[i] = new Asteroid(Math.floor(Math.random() * gameWidth), Math.floor(Math.random() * gameHeight), Math.floor(Math.random() * 6) - 3, Math.floor(Math.random() * 6) - 3, 50, 50);
+        this.asteroids[i] = new Asteroid(Math.floor(Math.random() * gameWidth), Math.floor(Math.random() * gameHeight), Math.floor(Math.random() * 6) - 3, Math.floor(Math.random() * 6) - 3, 50, 50);
       }
     }
 
@@ -95,52 +95,52 @@ $(document).ready(() => {
 
     checkKeys() {
       // up
-      if ((keyMap[87] || keyMap[38]) && player.x - player.speed * Math.cos(player.rotation) > 0 && player.x - player.speed * Math.cos(player.rotation) < gameWidth - player.width && player.y - player.speed * Math.sin(player.rotation) > 0 && player.y - player.speed * Math.sin(player.rotation) < gameHeight - player.height) {
-        player.x -= player.speed * Math.cos(player.rotation);
-        player.y -= player.speed * Math.sin(player.rotation);
+      if ((keyMap[87] || keyMap[38]) && this.player.x - this.player.speed * Math.cos(this.player.rotation) > 0 && this.player.x - this.player.speed * Math.cos(this.player.rotation) < gameWidth - this.player.width && this.player.y - this.player.speed * Math.sin(this.player.rotation) > 0 && this.player.y - this.player.speed * Math.sin(this.player.rotation) < gameHeight - this.player.height) {
+        this.player.x -= this.player.speed * Math.cos(this.player.rotation);
+        this.player.y -= this.player.speed * Math.sin(this.player.rotation);
       }
       // Right
       if (keyMap[68] || keyMap[39]) {
-        player.rotation += 0.1;
+        this.player.rotation += 0.1;
       }
       // Left
       if (keyMap[65] || keyMap[40]) {
-        player.rotation -= 0.1;
+        this.player.rotation -= 0.1;
       }
       // Down
-      if ((keyMap[83] || keyMap[37]) && player.x + player.speed * Math.cos(player.rotation) > 0 && player.x + player.speed * Math.cos(player.rotation) < gameWidth - player.width && player.y + player.speed * Math.sin(player.rotation) > 0 && player.y + player.speed * Math.sin(player.rotation) < gameHeight - player.height) {
-        player.x += player.speed * Math.cos(player.rotation);
-        player.y += player.speed * Math.sin(player.rotation);
+      if ((keyMap[83] || keyMap[37]) && this.player.x + this.player.speed * Math.cos(this.player.rotation) > 0 && this.player.x + this.player.speed * Math.cos(this.player.rotation) < gameWidth - this.player.width && this.player.y + this.player.speed * Math.sin(this.player.rotation) > 0 && this.player.y + this.player.speed * Math.sin(this.player.rotation) < gameHeight - this.player.height) {
+        this.player.x += this.player.speed * Math.cos(this.player.rotation);
+        this.player.y += this.player.speed * Math.sin(this.player.rotation);
       }
       // G
-      if (keyMap[71] && bullets.length < maxBullets) {
-        player.fire();
+      if (keyMap[71] && this.bullets.length < maxBullets) {
+        this.player.fire();
       }
     }
 
     drawHud() {
-      gameArea.drawText(`Score: ${player.score}`, 10, 20, 15);
+      gameArea.drawText(`Score: ${this.player.score}`, 10, 20, 15);
       gameArea.drawText('Lives: ', 10, 40, 15);
-      for (let i = 0; i < player.lives; i++) {
+      for (let i = 0; i < this.player.lives; i++) {
         gameArea.drawText('*', 60 + (10 * i), 40, 15);
       }
     }
 
     startScreen() {
       gameArea.clear();
-      gameArea.drawImg(gameWidth - 20, gameHeight, 0, 0, splashImg);
+      gameArea.drawImg(gameWidth - 20, gameHeight, 0, 0, this.splashImg);
       gameArea.drawText('Press E to play!', gameWidth / 2 - 40, (gameHeight / 2) + 60);
     }
 
     endScreen() {
       gameArea.clear();
       gameArea.drawText('Game Over!', gameWidth / 2 - 100, gameHeight / 2, 46);
-      gameArea.drawText(`Score: ${player.score}`, gameWidth / 2 - 50, gameHeight / 2 + 40, 24);
+      gameArea.drawText(`Score: ${this.player.score}`, gameWidth / 2 - 50, gameHeight / 2 + 40, 24);
     }
 
     update() {
-      if (!playing) {
-        if (player.lives < 0) {
+      if (!this.playing) {
+        if (this.player.lives < 0) {
 			    this.endScreen();
         } else {
 			    this.startScreen();
@@ -149,41 +149,42 @@ $(document).ready(() => {
         this.clear();
         this.checkKeys();
         this.drawHud();
-        player.draw();
-        if (asteroids.length > maxAsteroids) {
-			    asteroids.splice(0, maxAsteroids - asteroids.length);
+        this.player.draw();
+        if (this.asteroids.length > maxAsteroids) {
+			    this.asteroids.splice(0, maxAsteroids - this.asteroids.length);
         }
-        for (let i = 0; i < bullets.length; i++) {
-          bullets[i].move();
-          if (bullets[i]) {
-            bullets[i].draw();
+        for (let i = 0; i < this.bullets.length; i++) {
+          this.bullets[i].move();
+          if (this.bullets[i]) {
+            this.bullets[i].draw();
           }
         }
-        for (let i = 0; i < asteroids.length; i++) {
-          for (let j = 0; j < bullets.length; j++) {
-            if (bullets[j].collission(asteroids[i])) {
-              player.score++;
-              bullets[j].die();
-              asteroids[i].split();
+        for (let i = 0; i < this.asteroids.length; i++) {
+          for (let j = 0; j < this.bullets.length; j++) {
+            if (this.bullets[j].collission(this.asteroids[i])) {
+              this.player.score++;
+              this.bullets[j].die();
+              this.asteroids[i].split();
             }
           }
-          if (player.collission(asteroids[i])) {
-            if (asteroids[i].collided === false && player.lives > -1) {
-              player.lives--;
-            } else if (player.lives = 0) {
-              playing = false;
+          if (this.player.collission(this.asteroids[i])) {
+            if (this.asteroids[i].collided === false && this.player.lives > -1) {
+              this.player.lives--;
+            }
+            if (this.player.lives < 0) {
+              this.playing = false;
               continue;
             }
-            asteroids[i].split();
-            asteroids[i].collided = true;
-          } else if (asteroids[i].collided) {
-            asteroids[i].collided = false;
-          } else if (asteroids[i].height < 10) {
-            asteroids[i].die();
+            this.asteroids[i].split();
+            this.asteroids[i].collided = true;
+          } else if (this.asteroids[i].collided) {
+            this.asteroids[i].collided = false;
+          } else if (this.asteroids[i].height < 10) {
+            this.asteroids[i].die();
             continue;
           }
-          asteroids[i].move();
-          asteroids[i].draw();
+          this.asteroids[i].move();
+          this.asteroids[i].draw();
         }
       }
 	  }
@@ -198,6 +199,19 @@ $(document).ready(() => {
       this.rotation = rotation;
     }
 
+    setPos(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    checkBounds() {
+      if (this.x > 0 && (this.x + this.width) < gameWidth && this.y > 0 && (this.y + this.height) < gameHeight) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
     collission(gameObject) {
       // Standard bounding box collission detection
       let r1 = this.width + this.x;
@@ -209,7 +223,13 @@ $(document).ready(() => {
 		    return true;
       }
       return false;
-	  }
+    }
+    
+    randInBounds(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
   }
 
   class Player extends GameObject {
@@ -233,7 +253,7 @@ $(document).ready(() => {
     }
 
     fire() {
-      bullets.push(new Bullet(this.x, this.y + (this.height / 2), this.rotation));
+      gameArea.bullets.push(new Bullet(this.x, this.y + (this.height / 2), this.rotation));
     }
   }
 
@@ -243,50 +263,52 @@ $(document).ready(() => {
       this.collided = collided;
       this.xSpeed = xSpeed;
       this.ySpeed = ySpeed;
+      this.maxSpeed = 3
     }
 
     draw() {
-      gameArea.drawImg(this.width, this.height, this.x, this.y, asteroidImg, this.rotation);
+      gameArea.drawImg(this.width, this.height, this.x, this.y, gameArea.asteroidImg, this.rotation);
     }
 
     move() {
       if (this.x < 0 || this.x > gameWidth || this.y < 0 || this.y > gameHeight) {
         this.reset();
       } else if (this.xSpeed === 0 && this.ySpeed === 0) {
-        this.xSpeed = Math.floor(Math.random() * 6) - 3;
-        this.ySpeed = Math.floor(Math.random() * 6) - 3;
+        this.setSpeed(this.randInBounds(-3, 3), this.randInBounds(-3, 3));
       } else {
         this.x += this.xSpeed;
         this.y += this.ySpeed;
       }
     }
 
+    setVector(x, y, xSpeed, ySpeed) {
+      this.setPos(x, y);
+      this.setSpeed(xSpeed, ySpeed);
+    }
+
+    setSpeed(xSpeed, ySpeed) {
+      this.xSpeed = xSpeed;
+      this.ySpeed = ySpeed;
+    }
+
     reset() {
-      if (asteroids.length < maxAsteroids) {
-        switch (Math.floor(Math.random() * 4)) {
+      if (gameArea.asteroids.length < maxAsteroids) {
+        switch (this.randInBounds(1, 3)) {
           case 0:
-            this.x = 1;
-            this.y = Math.floor(Math.random() * gameHeight);
-            this.xSpeed = Math.floor(Math.random() * 3);
-            this.ySpeed = Math.floor(Math.random() * 6) - 3;
+            this.setPos(1, this.randInBounds(0, gameHeight));
+            this.setSpeed(this.randInBounds(0, this.maxSpeed), this.randInBounds(-this.maxSpeed, this.maxSpeed));
             break;
           case 1:
-            this.x = Math.floor(Math.random() * gameWidth);
-            this.y = gameHeight - 1;
-            this.xSpeed = Math.floor(Math.random() * 6) - 3;
-            this.ySpeed = Math.floor(Math.random() * 3);
+            this.setPos(this.randInBounds(0, gameWidth), gameHeight - 1);
+            this.setSpeed(this.randInBounds(-this.maxSpeed, this.maxSpeed), this.randInBounds(0, this.maxSpeed));
             break;
           case 2:
-            this.x = gameWidth - 1;
-            this.y = Math.floor(Math.random() * gameHeight);
-            this.xSpeed = Math.floor(Math.random() * 3) - 3;
-            this.ySpeed = Math.floor(Math.random() * 6) - 3;
+            this.setPos(gameWidth - 1, Math.floor(Math.random() * gameHeight));
+            this.setSpeed(Math.floor(Math.random() * 3) - 3, Math.floor(Math.random() * 6) - 3);
             break;
           case 3:
-            this.x = Math.floor(Math.random() * gameWidth);
-            this.y = 1;
-            this.xSpeed = Math.floor(Math.random() * 6) - 3;
-            this.ySpeed = Math.floor(Math.random() * 6) - 3;
+            this.setPos(Math.floor(Math.random() * gameWidth), 1);
+            this.setSpeed(Math.floor(Math.random() * 6) - 3, Math.floor(Math.random() * 6) - 3);
             break;
         }
         this.height = 50;
@@ -299,14 +321,14 @@ $(document).ready(() => {
 
     split() {
       if (!this.collided) {
-        asteroids.push(new Asteroid(this.x, this.y, -this.xSpeed, -this.ySpeed, this.height / 2, this.width / 2, true));
+        gameArea.asteroids.push(new Asteroid(this.x, this.y, -this.xSpeed, -this.ySpeed, this.height / 2, this.width / 2, true));
         this.height /= 2;
         this.width /= 2;
       }
     }
 
     die() {
-      asteroids.splice(asteroids.indexOf(this), 1);
+      gameArea.asteroids.splice(gameArea.asteroids.indexOf(this), 1);
     }
   }
 
@@ -332,7 +354,7 @@ $(document).ready(() => {
 
     die() {
       // Remove from bullets array
-      bullets.splice(bullets.indexOf(this), 1);
+      gameArea.bullets.splice(gameArea.bullets.indexOf(this), 1);
     }
   }
 
